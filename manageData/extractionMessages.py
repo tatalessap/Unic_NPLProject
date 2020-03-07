e0f329e6665b2fd706953bae9751780dcaf2b1b8
tatalessap <martapibirimp@gmail.com>
2020-03-07 18:12:55 +0100

    split done to do: clear list, clear the head

--- manageData/extractionMessages.py
+++ manageData/extractionMessages.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import re
 
-
 def createSetWords(messages, path, user1, user2):
     setWords = {user1: [], user2: []}
 
@@ -15,19 +14,27 @@ def createSetWords(messages, path, user1, user2):
                 setWords.get(user2).append(divText.text)
     return setWords
 
-"""
 def createClearList(set):
     listClearWords = []
 
     for word in set:
-        listClearWords = union(listClearWords, (word.split(" ")))
-        listClearWords = union(listClearWords, (word.split("\n")))
-
-    listClearWords = list(filter(lambda a: a != '', listClearWords))
+        listClearWords = union(listClearWords, totalSplit(word))
+        l#istClearWords = list(filter(lambda a: a != '', listClearWords))
+        #listClearWords = list(filter(lambda a: a != [1-2], listClearWords))
+        i = 0
 
     return listClearWords
 
 def union(lst1, lst2):
     final_list = list(set(lst1) | set(lst2))
     return final_list
-"""
+
+def totalSplit(word):
+    split = re.split('\n', word)
+    blank = ""
+    listWords = []
+    for eachWord in split:
+        if eachWord != blank:
+            clearList = re.split(' ', eachWord)
+            listWords = union(listWords, clearList)
+    return listWords
