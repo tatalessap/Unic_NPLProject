66dcbed56522afa9d3fc0a64fa6faceeaa7a6187
tatalessap <martapibirimp@gmail.com>
2020-03-11 15:44:05 +0100

    Fixed code structure, removed parts that were not needed. Generally to improve the cleaning of the set.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from manageData.toCreateSetWords import *
-
+"""
+Input to set, variables and name of files
+"""
 #list of messages, for example messages2.html
 messages = os.listdir('/home/tatalessap/PycharmProjects/NPLProject/Files')
 
@@ -10,18 +12,22 @@ user1 = 'Marta Pibiri'
 
 user2 = 'Stefano Raimondo Usa'
 
-listRemoveElements = ['MP', 'S']
-
 nameFileText = "Output.txt"
 
-#createFileWords(messages, pathMessages, nameFileText)
+##
+
+if not os.path.isfile("/home/tatalessap/PycharmProjects/NPLProject/"+nameFileText):
+    createFileWords(messages, pathMessages, nameFileText)
 
 setWords = createDocWords(nameFileText, user1, user2)
 
-clearList = createClearList(setWords.get(user1), listRemoveElements)
+clearList = createClearList(setWords.get(user1))
 
 wordsFile = open("Words.txt", "w")
 for el in clearList:
     wordsFile.write(el + "\n")
 wordsFile.close()
+
+
+
 
--- manageData/toCreateSetWords.py
+++ manageData/toCreateSetWords.py
@@ -1,16 +1,29 @@
 from bs4 import BeautifulSoup
 import re
+import os
 
-
+"""
+@:parameter messages: the name of the file, with the messages in html
+@:parameter path: position of the files
+@:parameter nameFileText: the name of the file where we save the messages
+"""
 def createFileWords(messages, path, nameFileText):
     for file in messages:
-        soup = BeautifulSoup(open(path + file), "html.parser")
+        soup = BeautifulSoup(open(path + file), "html.parser") #use soup for search and analyze the messages
         messagesFile = open(nameFileText, "w")
-        for i, divText in enumerate(soup.findAll("div")):
-            messagesFile.write(divText + "\n")
+        for i, divText in enumerate(soup.findAll("div")): #save the name user and the messages, to divide later
+            if divText.attrs.get('class')[0] == 'text' or divText.attrs.get('class')[0] == 'from_name':
+                t = re.sub(r'(?<!\.)', '', divText.text)
+                t = re.sub(r'       ', '', t)
+                messagesFile.write(t + "\n")
         messagesFile.close()
 
-
+"""
+@:parameter nameFileText: the name of the file which we take the messages
+@:parameter user1
+@:parameter user2
+In this methods we separate the message by user
+"""
 def createDocWords(nameFileText, user1, user2):
     setWords = {user1: [], user2: []}
     messagesFile = open(nameFileText, "r")
@@ -23,6 +36,7 @@ def createDocWords(nameFileText, user1, user2):
             flag = 1
         else:
             if line != '' and line != '\n':
+                #separate the message of user1 and user2
                 if flag == 0:
                     setWords.get(user1).append(line)
                 if flag == 1:
@@ -30,30 +44,34 @@ def createDocWords(nameFileText, user1, user2):
     messagesFile.close()
     return setWords
 
-
-def createClearList(set, listRemoveElements):
+"""
+@setUser: the set of messages by user
+create the list, much as possible "clear"
+"""
+def createClearList(setUser):
     listClearWords = []
 
-    for word in set:
+    for word in setUser:
+        #total split separate a word by blank / space and \n
         listClearWords = union(listClearWords, totalSplit(word))
 
     listClearWords = list(filter(lambda a: a != '', listClearWords))
-    for ele in listRemoveElements:
-        listClearWords = list(filter(lambda a: a != ele, listClearWords))
 
     return listClearWords
 
-
+"""
+@:parameter word: the word to split
+"""
 def totalSplit(word):
     split = re.split('\n', word)
     blank = ""
     listWords = []
     for eachWord in split:
         if eachWord != blank:
-            #eachWord = re.sub(r'(?<!\.)', ' ', eachWord)
             clearList = re.split(' ', eachWord)
             listWords = union(listWords, clearList)
     return listWords
+
 
 def union(lst1, lst2):
     final_list = list(set(lst1) | set(lst2))
