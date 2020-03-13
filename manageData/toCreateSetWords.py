ca52f22ec834d443191fb5f4194f3452b316277a
tatalessap <martapibirimp@gmail.com>
2020-03-13 16:06:33 +0100

    1660

--- manageData/toCreateSetWords.py
+++ manageData/toCreateSetWords.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import re
 import os
+import re
+import string
 
 """
 @:parameter messages: the name of the file, with the messages in html
@@ -9,14 +11,14 @@ import os
 """
 def createFileWords(messages, path, nameFileText):
     for file in messages:
-        soup = BeautifulSoup(open(path + file), "html.parser") #use soup for search and analyze the messages
+        soup = BeautifulSoup(open(path + '/' + file), "html.parser") #use soup for search and analyze the messages
         messagesFile = open(nameFileText, "w")
         for i, divText in enumerate(soup.findAll("div")): #save the name user and the messages, to divide later
             if divText.attrs.get('class')[0] == 'text' or divText.attrs.get('class')[0] == 'from_name':
                 t = re.sub(r'(?<!\.)', '', divText.text)
                 t = re.sub(r'       ', '', t)
                 messagesFile.write(t + "\n")
-        messagesFile.close()
+    messagesFile.close()
 
 """
 @:parameter nameFileText: the name of the file which we take the messages
@@ -48,14 +50,18 @@ def createDocWords(nameFileText, user1, user2):
 @setUser: the set of messages by user
 create the list, much as possible "clear"
 """
-def createClearList(setUser):
+def createClearList(setUser, listToEliminateSymbols):
     listClearWords = []
 
     for word in setUser:
         #total split separate a word by blank / space and \n
         listClearWords = union(listClearWords, totalSplit(word))
 
-    listClearWords = list(filter(lambda a: a != '', listClearWords))
+    for el in listToEliminateSymbols:
+        listClearWords = list(filter(lambda x: not (el in x), listClearWords))
+
+    listClearWords = list(filter(lambda x: not (len(x) == 1), listClearWords))
+    listClearWords = list(filter(lambda x: not (len(x) > 26), listClearWords))
 
     return listClearWords
 
@@ -63,10 +69,13 @@ def createClearList(setUser):
 @:parameter word: the word to split
 """
 def totalSplit(word):
+    remove = string.punctuation
+    pattern = r"[{}]".format(remove)  # create the pattern
     split = re.split('\n', word)
     blank = ""
     listWords = []
     for eachWord in split:
+        eachWord = re.sub(pattern, "", eachWord)
         if eachWord != blank:
             clearList = re.split(' ', eachWord)
             listWords = union(listWords, clearList)
